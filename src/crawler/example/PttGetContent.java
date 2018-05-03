bf5f604cf103e256d196fdb9ce0c120d418a571b
p1017e1015 <p1017e1015@gmail.com>
2018-05-03 16:34:32 +0800

    homework1

---
+++ src/crawler/example/PttGetContent.java
@@ -0,0 +1,31 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.jsoup.nodes.Document;
+
+
+/**
+ * 簡易練習
+ * 
+ * 找出所有文章中按推的id
+ * 
+ * @author Abola Lee
+ *
+ */
+public class PttGetContent {
+	
+	public static void main(String[] args) {
+		String uri = "" +
+				"" +
+				"" +
+				"l";
+
+		Document jsoupObject = CrawlerPack.start().addCookie("over18", "1").getFromHtml(uri);
+
+		jsoupObject.select("#main-content div").remove();
+		jsoupObject.select("#main-content span").remove();
+
+
+		System.out.println( jsoupObject.select("#main-content").text());
+	}
+}
